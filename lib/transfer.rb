cd728de2af243e47b9066793132a59062a7fd4c4
Kevin J Gomez <kjgmez1@gmail.com>
2018-11-06 05:42:41 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
       self.receiver.balance = self.receiver.balance + self.amount
       self.status = "complete"
     else
-      "Transaction rejected. Please check your account balance."
+      return "Transaction rejected. Please check your account balance."
       self.status = "rejected"
     end
   end
